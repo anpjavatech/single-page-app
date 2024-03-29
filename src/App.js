a7ccb1012fb196b91ecae166c68dc53f98f279b2
Anoop Kuttikattu Sukumaran Nair <anpks@Anoops-Air.box>
2024-03-29 10:01:56 +0100

    Commit after adding the initial commit for single page application

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+
+
+function App() {
+
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element: <HomePage />,
+    },
+    {
+      path:"/products",
+      element: <ProductsPage />,
+    },
+  ]);
+
+  return  <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/pages/Products.js
@@ -0,0 +1,3 @@
+export default function ProductsPage(){
+    return <h1>My Products Page.</h1>
+}
